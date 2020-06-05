381c3f59160f57917ac308699d97c5c150816f04
mickael morel <noxbike@gmail.com>
2020-06-05 16:09:23 +0000

    design ajouter

--- function.js
+++ function.js
@@ -6,18 +6,45 @@ function readForm(array){
     for(element of array){
         $(element).each(function(){
             var name = $(this).attr("class");
-            var obj = {name: name, result: $(`.${name}`).val()};
-            infoForm.push(obj);
+            infoForm.push({name: name, result: $(`.${name}`).val()});
         })
     }
     return infoForm;
+}
+
+function checkForm(array){
+    var resultat = { messageError: '', validEmail: '' };
+
+    for(element of array){
+        //if result is empty add input class in error message
+        if(!element.result){
+            resultat.messageError += ` ${element.name}` ;
+            dangerBorder(element.name, element.result);
+        }
+        //if result is not empty and input class is email
+        //then check if email is correct
+        else if(element.name == 'email' && element.result){
+            resultat.validEmail = validateEmail(element.result);
+            dangerBorder(element.name, !resultat.validEmail);
+        }
+
+        else{
+            dangerBorder(element.name, true);
+        }
+    }
+
+    return resultat;
+}
+
+function dangerBorder(element, boolean){
+   var resultat = boolean ? 'green' : 'red';
+   $(`.${element}`).css('border', `1px solid ${resultat}`)
 }
 
 //verify if email is a real email
 function validateEmail(email)
 {
     var regexEmail = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
-    var exe = regexEmail.test(email);
-    exe = !exe ? '<p style="color: red;">Votre email est invalide</p>': ' ';
-    return exe;
+    var resultat = !regexEmail.test(email) ? '<p  class="alert alert-danger" role="alert" >Votre email est invalide</p>': '';
+    return resultat;
 }
--- script.js
+++ script.js
@@ -1,30 +1,23 @@
 $(function(){
     $('#submit').click(function(){
         const donnees = ['input', 'textarea'];
-        var valemail = '';
-        var messageError="";
-        
-        var infoForm = readForm(donnees);
+        var textToShow;
 
-        for(element of infoForm){
-            //if result is empty add input class in error message
-            if(!element.result){
-                messageError += ` ${element.name}` ;
-            }
-            //if result is not empty and input class is email
-            //then check if email is correct
-            else if(element.name == 'email' && element.result){
-                valemail = validateEmail(element.result);
-            }
-        }
+        var infoForm = readForm(donnees);
+        var message = checkForm(infoForm);
+
         //if messageError is not empty add all input name with text
-        messageError = messageError ? `<p style='color: red;'>veuillez remplir le(s) champ(s) suivant: ${messageError}</p>`: '' ;
-        //if messageError is empty and valemail is empty
-        //then messageError must confirm message
-        //or messageError and valemail must be show 
-        messageError = !messageError && !valemail ? `<p style='color: green;'>Votre message a bien été envoyé</p>`: `${messageError}  ${valemail}`;
-        
-        $('#message-error').html(messageError);
-        return false; 
+        var messageError = message.messageError ? `<p class="alert alert-danger" role="alert" >veuillez remplir le(s) champ(s) suivant: ${message.messageError}</p>`: '' ;
+        //if messageError is empty and validEmail is empty
+        //then messageError must confirm message or messageError and validEmail must be show
+        textToShow = !messageError && !message.validEmail ? true : false ;
+
+        if(!textToShow){
+            $('#message-error').html(`${messageError}  ${message.validEmail}`);
+            return false;
+        }
+        else{
+            $('#message-error').html('<p class="alert alert-success" role="alert">Votre message a bien été envoyé</p>');
+        }
     })
 })
